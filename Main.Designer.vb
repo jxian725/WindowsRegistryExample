993fece3ab8cc2a690c297334b2e10279a79f280
Jing Xian <jingxian_98@hotmail.com>
2020-11-02 12:09:36 +0800

    Add project files.

---
+++ Main.Designer.vb
@@ -0,0 +1,134 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBoxRName = New System.Windows.Forms.TextBox()
+        Me.ButtonCreate = New System.Windows.Forms.Button()
+        Me.ButtonCreateChild = New System.Windows.Forms.Button()
+        Me.TextBoxChild = New System.Windows.Forms.TextBox()
+        Me.TextBoxChildValue = New System.Windows.Forms.TextBox()
+        Me.TextBoxRead = New System.Windows.Forms.TextBox()
+        Me.ButtonRead = New System.Windows.Forms.Button()
+        Me.TextBoxReadChild = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'TextBoxRName
+        '
+        Me.TextBoxRName.Location = New System.Drawing.Point(24, 23)
+        Me.TextBoxRName.Name = "TextBoxRName"
+        Me.TextBoxRName.Size = New System.Drawing.Size(172, 20)
+        Me.TextBoxRName.TabIndex = 0
+        Me.TextBoxRName.Text = "Please Enter Registry Name"
+        Me.TextBoxRName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'ButtonCreate
+        '
+        Me.ButtonCreate.Location = New System.Drawing.Point(208, 21)
+        Me.ButtonCreate.Name = "ButtonCreate"
+        Me.ButtonCreate.Size = New System.Drawing.Size(115, 23)
+        Me.ButtonCreate.TabIndex = 1
+        Me.ButtonCreate.Text = "Create New Entry"
+        Me.ButtonCreate.UseVisualStyleBackColor = True
+        '
+        'ButtonCreateChild
+        '
+        Me.ButtonCreateChild.Location = New System.Drawing.Point(208, 79)
+        Me.ButtonCreateChild.Name = "ButtonCreateChild"
+        Me.ButtonCreateChild.Size = New System.Drawing.Size(115, 23)
+        Me.ButtonCreateChild.TabIndex = 2
+        Me.ButtonCreateChild.Text = "Create Child"
+        Me.ButtonCreateChild.UseVisualStyleBackColor = True
+        '
+        'TextBoxChild
+        '
+        Me.TextBoxChild.Location = New System.Drawing.Point(24, 67)
+        Me.TextBoxChild.Name = "TextBoxChild"
+        Me.TextBoxChild.Size = New System.Drawing.Size(172, 20)
+        Me.TextBoxChild.TabIndex = 3
+        Me.TextBoxChild.Text = "Please Enter Child Name"
+        Me.TextBoxChild.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBoxChildValue
+        '
+        Me.TextBoxChildValue.Location = New System.Drawing.Point(24, 93)
+        Me.TextBoxChildValue.Name = "TextBoxChildValue"
+        Me.TextBoxChildValue.Size = New System.Drawing.Size(172, 20)
+        Me.TextBoxChildValue.TabIndex = 4
+        Me.TextBoxChildValue.Text = "Please Enter Child Value"
+        Me.TextBoxChildValue.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBoxRead
+        '
+        Me.TextBoxRead.Location = New System.Drawing.Point(24, 188)
+        Me.TextBoxRead.Name = "TextBoxRead"
+        Me.TextBoxRead.Size = New System.Drawing.Size(172, 20)
+        Me.TextBoxRead.TabIndex = 5
+        Me.TextBoxRead.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'ButtonRead
+        '
+        Me.ButtonRead.Location = New System.Drawing.Point(208, 171)
+        Me.ButtonRead.Name = "ButtonRead"
+        Me.ButtonRead.Size = New System.Drawing.Size(115, 23)
+        Me.ButtonRead.TabIndex = 6
+        Me.ButtonRead.Text = "Read Registry"
+        Me.ButtonRead.UseVisualStyleBackColor = True
+        '
+        'TextBoxReadChild
+        '
+        Me.TextBoxReadChild.Location = New System.Drawing.Point(24, 162)
+        Me.TextBoxReadChild.Name = "TextBoxReadChild"
+        Me.TextBoxReadChild.Size = New System.Drawing.Size(172, 20)
+        Me.TextBoxReadChild.TabIndex = 7
+        Me.TextBoxReadChild.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(350, 261)
+        Me.Controls.Add(Me.TextBoxReadChild)
+        Me.Controls.Add(Me.ButtonRead)
+        Me.Controls.Add(Me.TextBoxRead)
+        Me.Controls.Add(Me.TextBoxChildValue)
+        Me.Controls.Add(Me.TextBoxChild)
+        Me.Controls.Add(Me.ButtonCreateChild)
+        Me.Controls.Add(Me.ButtonCreate)
+        Me.Controls.Add(Me.TextBoxRName)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Main"
+        Me.Text = "Windows Registry"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBoxRName As TextBox
+    Friend WithEvents ButtonCreate As Button
+    Friend WithEvents ButtonCreateChild As Button
+    Friend WithEvents TextBoxChild As TextBox
+    Friend WithEvents TextBoxChildValue As TextBox
+    Friend WithEvents TextBoxRead As TextBox
+    Friend WithEvents ButtonRead As Button
+    Friend WithEvents TextBoxReadChild As TextBox
+End Class
---
+++ Main.vb
@@ -0,0 +1,59 @@
+﻿Public Class Main
+    Private Sub Main_Load(sender As Object, e As EventArgs) Handles Me.Load
+        'ButtonCreateChild.Enabled = False
+    End Sub
+
+    Dim NewName As String
+    Private Sub TextBoxRName_Click(sender As Object, e As EventArgs) Handles TextBoxRName.Click
+        TextBoxRName.Text = ""
+    End Sub
+
+    Private Sub ButtonCreate_Click(sender As Object, e As EventArgs) Handles ButtonCreate.Click
+        If TextBoxRName.Text = "Please Enter Registry Name" Then
+            MsgBox("Please Enter Registry Name")
+            TextBoxRName.Text = ""
+        ElseIf TextBoxRName.Text = "" Then
+            MsgBox("Please Enter Registry Name")
+        Else
+            NewName = TextBoxRName.Text
+            My.Computer.Registry.CurrentUser.CreateSubKey(NewName)
+            MsgBox("Parent Created")
+            ButtonCreateChild.Enabled = True
+        End If
+    End Sub
+
+    Dim RegisLoc As String = "HKEY_CURRENT_USER\" + NewName
+    Private Sub ButtonCreateChild_Click(sender As Object, e As EventArgs) Handles ButtonCreateChild.Click
+        If TextBoxChild.Text = "" Then
+            MsgBox("Please Enter Child Name")
+        ElseIf TextBoxChild.Text = "Please Enter Child Name" Then
+            MsgBox("Please Enter Child Name")
+            TextBoxChild.Text = ""
+        Else
+            If TextBoxChildValue.Text = "" Then
+                MsgBox("Please Enter Child Value")
+            ElseIf TextBoxChildValue.Text = "Please Enter Child Value" Then
+                MsgBox("Please Enter Child value")
+                TextBoxChild.Text = ""
+            Else
+                My.Computer.Registry.SetValue(RegisLoc, TextBoxChild.Text, TextBoxChildValue.Text)
+                MsgBox("Pair Created")
+            End If
+        End If
+    End Sub
+
+    Private Sub ButtonRead_Click(sender As Object, e As EventArgs) Handles ButtonRead.Click
+        If TextBoxRead.Text = "" Then
+            MsgBox("Please enter value to read")
+        Else
+            If TextBoxReadChild.Text = "" Then
+                MsgBox("Please enter value to read")
+            Else
+                Dim inpputval As String = TextBoxRead.Text
+                Dim inputchild As String = "HKEY_CURRENT_USER\" + TextBoxReadChild.Text
+                Dim readValue = My.Computer.Registry.GetValue(inputchild, inpputval, Nothing)
+                MsgBox("Value :" & readValue)
+            End If
+        End If
+    End Sub
+End Class
